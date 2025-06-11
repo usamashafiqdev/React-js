69aa115843a0db539b621b8340f500e20917bc08
usamashafiqdev <itusamashafiq@gmail.com>
2025-06-11 18:34:17 +0500

    Add ToDos List

--- redux-tk/src/App.jsx
+++ redux-tk/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './Components/AddToDo'
+import Todos from './Components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,9 @@ function App() {
     <>
    
     <h1>Redux</h1>
+    <AddTodo></AddTodo>
+    <Todos></Todos>
+
     </>
   )
 }
